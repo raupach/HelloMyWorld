6b78786ffe1ff010ef3a3e9ce4b52f2bdb0e1772
Oliver Raupach <oliver@raupach.cc>
2020-04-01 11:10:39 +0200

    4

--- src/main/java/cc/raupach/App.java
+++ src/main/java/cc/raupach/App.java
@@ -2,12 +2,9 @@ package cc.raupach;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
